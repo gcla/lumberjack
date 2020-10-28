45dce8ac2e95f29adee3a013907ef1b9877afe74
rmadapur <rmadapur@extremenetworks.ccom>
2020-10-28 10:10:23 +0530

    Add redirect stderr support on windows

---
+++ redirect_stderr_unix.go
@@ -0,0 +1,17 @@
+// Log the panic under unix to the log file
+// +build !windows,!solaris,!plan9,!js
+
+package lumberjack
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+// redirectStderr to the file passed in
+func redirectStderr(f *os.File) {
+	if err:=syscall.Dup2(int(f.Fd()), syscall.Stderr);err!=nil{
+		fmt.Errorf("can't dup2 logfile and stderr: %s", err)
+	}
+}
